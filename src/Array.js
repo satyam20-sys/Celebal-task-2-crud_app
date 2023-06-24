14d23a788351ad3677993472c8161dd10fa55f77
Satyam Sinha <sinhasatyam662@gmail.com>
2023-06-24 11:59:15 +0530

    added files

---
+++ src/Array.js
@@ -0,0 +1,19 @@
+const array = [
+    {
+        id:'1', 
+        Name:'Study',
+        Age:'11 to 12 pm'
+    },
+    {
+        id:'2', 
+        Name:'Gym',
+        Age:'5 pm'
+    },
+    {
+        id:'3',
+        Name:'Football',
+        Age:'7 pm'
+    }
+]
+  
+export default array
